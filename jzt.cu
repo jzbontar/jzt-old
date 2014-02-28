811f6b6131a022782f9e104aa19e94f2d31d3746
Jure Zbontar <jure.zbontar@gmail.com>
2014-02-28 19:34:52 +0100

    mat_vect

--- jzt.cu
+++ jzt.cu
@@ -7,12 +7,77 @@ extern "C" {
 #include "luaT.h"
 #include "THC.h"
 
+#include <thrust/transform.h>
+#include <thrust/device_ptr.h>
+
 #include <stdio.h>
 #include <assert.h>
 #include "cublas_v2.h"
 
 #define TB 128
 
+/* operations */
+struct opPlus {
+    static const float base_value = 0.0;
+    __device__ float operator()(float x, float y)
+    {
+        return x + y;
+    }
+};
+
+struct opMinus {
+    static const float base_value = 0.0;
+    __device__ float operator()(float x, float y)
+    {
+        return x - y;
+    }
+};
+
+struct opMult {
+    static const float base_value = 1.0;
+    __device__ float operator()(float x, float y)
+    {
+        return x * y;
+    }
+};
+
+struct opDiv {
+    static const float base_value = 1.0;
+    __device__ float operator()(float x, float y)
+    {
+        return x / y;
+    }
+};
+
+struct opShrink {
+	float threshold;
+	opShrink(float threshold_) : threshold(threshold_) {};
+	
+	__device__ float operator()(float x) { 
+		if (x - threshold > 0) {
+			return x - threshold;
+		} else if (x + threshold < 0) {
+			return x + threshold;
+		} else {
+			return 0.0;
+		}
+	}
+};
+
+/* Is A in row major format? */
+int is_rm(THCudaTensor *A)
+{
+	return A->stride[1] == 1;
+}
+
+void checkCudaError(lua_State *L) {
+	cudaError_t status = cudaPeekAtLastError();
+	if (status != cudaSuccess) {
+		luaL_error(L, cudaGetErrorString(status));
+	}
+}
+
+/* res[i] = A[inds[i]] */
 __global__ void get_cols(float *A, int A_stride, float *inds, float *res, int len)
 {
 	int i = blockIdx.x * blockDim.x + threadIdx.x;
@@ -23,7 +88,7 @@ __global__ void get_cols(float *A, int A_stride, float *inds, float *res, int le
 
 int get_cols(lua_State *L)
 {
-    THCudaTensor *A = (THCudaTensor*)luaT_checkudata(L, 1, "torch.CudaTensor");
+	THCudaTensor *A = (THCudaTensor*)luaT_checkudata(L, 1, "torch.CudaTensor");
 	THCudaTensor *inds = (THCudaTensor*)luaT_checkudata(L, 2, "torch.CudaTensor");
 	THCudaTensor *res = (THCudaTensor*)luaT_checkudata(L, 3, "torch.CudaTensor");
 	int len = THCudaTensor_nElement(inds);
@@ -31,6 +96,8 @@ int get_cols(lua_State *L)
 	return 0;
 }
 
+
+/* A[inds[i]] = val */
 __global__ void set_cols(float *A, int A_stride, float *inds, float val, int len)
 {
 	int i = blockIdx.x * blockDim.x + threadIdx.x;
@@ -41,7 +108,7 @@ __global__ void set_cols(float *A, int A_stride, float *inds, float val, int len
 
 int set_cols(lua_State *L)
 {
-    THCudaTensor *A = (THCudaTensor*)luaT_checkudata(L, 1, "torch.CudaTensor");
+	THCudaTensor *A = (THCudaTensor*)luaT_checkudata(L, 1, "torch.CudaTensor");
 	THCudaTensor *inds = (THCudaTensor*)luaT_checkudata(L, 2, "torch.CudaTensor");
 	float val = luaL_checknumber(L, 3);
 	int len = THCudaTensor_nElement(inds);
@@ -49,10 +116,100 @@ int set_cols(lua_State *L)
 	return 0;
 }
 
+
+int shrink(lua_State *L)
+{
+	THCudaTensor *A = (THCudaTensor*)luaT_checkudata(L, 1, "torch.CudaTensor");
+	THCudaTensor *B = (THCudaTensor*)luaT_checkudata(L, 2, "torch.CudaTensor");
+	float threshold = luaL_checknumber(L, 3);
+
+	thrust::device_ptr<float> Ap(THCudaTensor_data(A));
+	thrust::device_ptr<float> Bp(THCudaTensor_data(B));
+	thrust::transform(Ap, Ap + THCudaTensor_nElement(A), Bp, opShrink(threshold));
+
+	return 0;
+}
+
+
+/* What a crazy bug!
+ *
+ *
+ *
+ *
+ *
+ */
+template <class Op, int axis>
+__global__ void kMatVect(Op op, float *A, float *x, float *B, int len, int size0)
+{
+	int i = blockIdx.x * blockDim.x + threadIdx.x;
+	if (i < len) {
+		if (axis == 0) B[i] = op(A[i], x[i % size0]);
+		if (axis == 1) B[i] = op(A[i], x[i / size0]);
+	}
+}
+
+template <class Op>
+int mat_vect(Op op, lua_State *L)
+{
+	THCudaTensor *A = (THCudaTensor*)luaT_checkudata(L, 1, "torch.CudaTensor");
+	THCudaTensor *x = (THCudaTensor*)luaT_checkudata(L, 2, "torch.CudaTensor");
+	THCudaTensor *B = (THCudaTensor*)luaT_checkudata(L, 3, "torch.CudaTensor");
+	int axis = luaL_checkint(L, 4);
+
+	if (!is_rm(A) || !is_rm(B)) {
+		luaL_error(L, "Matrix not in row major order");
+	}
+
+	if (THCudaTensor_nElement(A) != THCudaTensor_nElement(B)) {
+		luaL_error(L, "Size mismatch");
+	}
+
+	int len = THCudaTensor_nElement(A);
+	if (axis == 0) {
+		if (A->size[1] != THCudaTensor_nElement(x)) {
+			luaL_error(L, "Size mismatch");
+		}
+		kMatVect<Op, 0><<<(len - 1) / TB + 1, TB>>>(op, THCudaTensor_data(A), THCudaTensor_data(x), THCudaTensor_data(B), len, A->size[1]);
+	} else if (axis == 1) {
+		if (A->size[0] != THCudaTensor_nElement(x)) {
+			luaL_error(L, "Size mismatch");
+		}
+		kMatVect<Op, 1><<<(len - 1) / TB + 1, TB>>>(op, THCudaTensor_data(A), THCudaTensor_data(x), THCudaTensor_data(B), len, A->size[1]);
+	}
+
+	checkCudaError(L);
+	return 0;
+}
+
+int add_mat_vect(lua_State *L)
+{
+	return mat_vect(opPlus(), L);
+}
+
+int sub_mat_vect(lua_State *L)
+{
+	return mat_vect(opMinus(), L);
+}
+
+int mult_mat_vect(lua_State *L)
+{
+	return mat_vect(opMult(), L);
+}
+
+int div_mat_vect(lua_State *L)
+{
+	return mat_vect(opDiv(), L);
+}
+
+
 static const struct luaL_Reg funcs[] = {
 	{"get_cols", get_cols},
 	{"set_cols", set_cols},
-
+	{"shrink", shrink},
+	{"add_mat_vect", add_mat_vect},
+	{"sub_mat_vect", sub_mat_vect},
+	{"mult_mat_vect", mult_mat_vect},
+	{"div_mat_vect", div_mat_vect},
 	{NULL, NULL}
 };
 
