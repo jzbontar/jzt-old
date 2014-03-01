306a12999c1aed56ef3b1789ae3c6eeab23a83d2
Jure Zbontar <jure.zbontar@gmail.com>
2014-03-01 17:47:03 +0100

    Optional bias

--- Linear.lua
+++ Linear.lua
@@ -2,14 +2,18 @@ require 'jzt'
 
 local Linear, parent = torch.class('jzt.Linear', 'nn.Module')
 
-function Linear:__init(inputSize, outputSize)
+function Linear:__init(inputSize, outputSize, doBias)
    parent.__init(self)
    self:cuda()
 
+   self.doBias = doBias == nil and true or doBias
+
    self.weight = torch.CudaTensor(outputSize, inputSize)
-   self.bias = torch.CudaTensor(outputSize)
    self.gradWeight = torch.CudaTensor(outputSize, inputSize)
-   self.gradBias = torch.CudaTensor(outputSize)
+   if self.doBias then
+      self.bias = torch.CudaTensor(outputSize)
+      self.gradBias = torch.CudaTensor(outputSize)
+   end
 
    self:reset()
 end
@@ -17,16 +21,20 @@ end
 function Linear:reset()
    stdv = 1 / math.sqrt(self.weight:size(2))
    self.weight:uniform(-stdv, stdv)
-   self.bias:uniform(-stdv, stdv)
+   if self.doBias then
+      self.bias:uniform(-stdv, stdv)
+   end
 end
 
 function Linear:updateOutput(input)
    local nframe = input:size(1)
-   local nunit = self.bias:size(1)
+   local nunit = self.weight:size(1)
 
    self.output:resize(nframe, nunit)
    self.output:addmm(0, 1, input, self.weight:t())
-   jzt.add_mat_vect(self.output, self.bias, self.output, 1)
+   if self.doBias then
+      jzt.add_mat_vect(self.output, self.bias, self.output, 1)
+   end
    return self.output
 end
 
@@ -38,5 +46,7 @@ end
 
 function Linear:accGradParameters(input, gradOutput)
    self.gradWeight:addmm(0, 1, gradOutput:t(), input)
-   jzt.sum(gradOutput, self.gradBias, 1)
+   if self.doBias then
+      jzt.sum(gradOutput, self.gradBias, 1)
+   end
 end
