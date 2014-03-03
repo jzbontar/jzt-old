c89b4394ecda0ca50fe87589ea96a172bb2ae1c4
Jure Zbontar <jure.zbontar@gmail.com>
2014-03-03 16:43:11 +0100

    Simplification and test

--- ClassNLLCriterion.lua
+++ ClassNLLCriterion.lua
@@ -2,24 +2,18 @@ local ClassNLLCriterion, parent = torch.class('jzt.ClassNLLCriterion', 'nn.Crite
 
 function ClassNLLCriterion:__init()
    parent.__init(self)
+   self:cuda()
    self.sizeAverage = true
    self.tmp = torch.CudaTensor()
 end
 
 function ClassNLLCriterion:updateOutput(input, target)
-   if input:dim() == 1 then
-      self.output = -input[target]
-   elseif input:dim() == 2 then
-      self.tmp:resizeAs(target)
-      jzt.get_cols(input, target, self.tmp)
-      local output = -self.tmp:sum()
+   self.tmp:resizeAs(target)
+   jzt.get_cols(input, target, self.tmp)
+   self.output = -self.tmp:sum()
 
-      if self.sizeAverage then
-         output = output / target:size(1)
-      end
-      self.output = output
-   else
-      error('matrix or vector expected')
+   if self.sizeAverage then
+      self.output = self.output / target:size(1)
    end
    return self.output
 end
@@ -28,15 +22,11 @@ function ClassNLLCriterion:updateGradInput(input, target)
    self.gradInput:resizeAs(input)
    self.gradInput:zero()
 
-  if input:dim() == 1 then
-      self.gradInput[target] = -1
-   else
-      local z = -1
-      if self.sizeAverage then
-         z = z / target:size(1)
-      end
-      jzt.set_cols(self.gradInput, target, z)
+   local z = -1
+   if self.sizeAverage then
+      z = z / target:size(1)
    end
+   jzt.set_cols(self.gradInput, target, z)
 
    return self.gradInput
 end
---
+++ test.lua
@@ -0,0 +1,29 @@
+require 'jzt'
+require 'Test'
+
+test = {}
+function test.HuberCost()
+   A = torch.rand(5, 3):cuda()
+   B = torch.rand(5, 3):cuda()
+
+   assert(testCriterion(jzt.HuberCost(1), A, B) < 1e-4)
+end
+
+function test.KLDivergence()
+   A = torch.rand(5, 3):cuda():log()
+   B = torch.rand(5, 3):cuda():log()
+
+   assert(testCriterion(jzt.KLDivergence(), A, B) < 1e-3)
+end
+
+function test.ClassNLLCriterion()
+   A = torch.rand(5, 3):cuda()
+   B = torch.eye(3):index(1, torch.LongTensor{1,2,3,1,2}):cuda()
+
+   assert(testCriterion(jzt.ClassNLLCriterion(), A, B) < 1e-4)
+end
+
+for k, v in pairs(test) do
+   print('Testing ' .. k)
+   v()
+end
