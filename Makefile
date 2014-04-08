70d2bdd696218818d6d2ecd0c3fc8995ae90eba8
Jure Zbontar <jure.zbontar@gmail.com>
2014-04-08 23:25:43 +0200

    pulled SpatialLogSoftMax from torch

--- Makefile
+++ Makefile
@@ -2,8 +2,16 @@ PREFIX=/home/jure/build/torch7
 CFLAGS=-I$(PREFIX)/include/THC -I$(PREFIX)/include/TH -I$(PREFIX)/include
 LDFLAGS=-L$(PREFIX)/lib -Xlinker -rpath,$(PREFIX)/lib -lcublas -lluaT -lTHC -lTH
 
-libjzt.so: jzt.cu
-	nvcc -arch sm_35 --compiler-options '-fPIC' -o libjzt.so --shared jzt.cu $(CFLAGS) $(LDFLAGS)
+OBJ = jzt.o SpatialLogSoftMax.o
+
+%.o : %.cu
+	nvcc -arch sm_35 --compiler-options '-fPIC' -c $(CFLAGS) $<
+
+libjzt.so: ${OBJ}
+	nvcc -o libjzt.so --shared ${OBJ} $(LDFLAGS)
 
 install: libjzt.so
 	ln -s /home/jure/torch/jzt/libjzt.so /home/jure/build/torch/installed/lib/torch/lua/
+
+clean:
+	rm -f *.o *.so
---
+++ SpatialLogSoftMax.lua
@@ -0,0 +1,20 @@
+local SpatialLogSoftMax,parent = torch.class('nn.SpatialLogSoftMax', 'nn.Module')
+
+function SpatialLogSoftMax:__init(constant)
+   parent.__init(self)
+   self.constant_present = false
+   self.constant = constant -- a constant added to the exponential sum
+   if self.constant then
+      print('Using exp('..self.constant
+	    ..') as additional denomimator constant in SpatialLogSoftMax')
+      self.constant_present = true
+   end
+end
+
+function SpatialLogSoftMax:updateOutput(input)
+   return input.nn.SpatialLogSoftMax_updateOutput(self, input)
+end
+
+function SpatialLogSoftMax:updateGradInput(input, gradOutput)
+   return input.nn.SpatialLogSoftMax_updateGradInput(self, input, gradOutput)
+end
