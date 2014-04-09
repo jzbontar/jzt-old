a061649590ecfbeff29cf5b004affaae69148b20
Jure Zbontar <jure.zbontar@gmail.com>
2014-04-09 06:13:14 +0200

    fix test for criterion on GPU

--- Test.lua
+++ Test.lua
@@ -54,10 +54,10 @@ function testCriterion(module, input, target)
    for i = 1,sinput:nElement() do
       local orig = sinput[i]
       sinput[i] = orig + eps
-      local f1 = module:forward(input, target)
+      local f1 = module:forward(input, target):double()
 
       sinput[i] = orig - eps
-      local f2 = module:forward(input, target)
+      local f2 = module:forward(input, target):double()
 
       grad_hat[i] = (f1 - f2) / (2 * eps)
       sinput[i] = orig
