5d2555dfc7c13ca227ab7c21b569364645f6e500
Jure Zbontar <jure.zbontar@gmail.com>
2014-04-13 03:28:03 +0200

    add savePNG

--- util.lua
+++ util.lua
@@ -1,3 +1,36 @@
+function jzt.savePNG(fname, tensor, jet)
+   local n_images, height, width
+
+   if tensor:dim() == 2 then
+      n_images = 1
+      height = tensor:size(1)
+      width = tensor:size(2)
+   elseif tensor:dim() == 3 then
+      n_images = tensor:size(1)
+      height = tensor:size(2)
+      width = tensor:size(3)
+   elseif tensor:dim() == 4 then
+      n_images = tensor:size(1)
+      height = tensor:size(3)
+      width = tensor:size(4)
+   end
+
+   local x = torch.Tensor(n_images, height, width):copy(tensor)
+   for i = 1,n_images do
+      x[i]:add(-x[i]:min()):div(x[i]:max() - x[i]:min())
+   end
+
+   x:resize(n_images * height, width)
+
+   if jet then
+      col = torch.Tensor(3, n_images * height, width)
+      jzt.grey2jet(x, col)
+      x = col
+   end
+
+   image.savePNG(fname, x)
+end
+
 -- B = softmax(A), x is temporary storage
 function jzt.softmax(A, B, x)
    jzt.max(A, x, 2)
