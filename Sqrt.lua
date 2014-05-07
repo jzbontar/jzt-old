b98faa107e4e00c1a79afcebd7605bd038f1b9aa
Jure Zbontar <jure.zbontar@gmail.com>
2014-05-07 21:38:31 +0200

    fix Sqrt backprop

--- Sqrt.lua
+++ Sqrt.lua
@@ -2,14 +2,16 @@ local Sqrt, parent = torch.class('jzt.Sqrt','nn.Module')
 
 function Sqrt:__init()
    parent.__init(self)
+   self.output_clone = torch.CudaTensor()
 end
 
 function Sqrt:updateOutput(input)
    self.output = input:sqrt()
+   self.output_clone:resizeAs(self.output):copy(self.output)
    return self.output
 end
 
 function Sqrt:updateGradInput(input, gradOutput)
-   self.gradInput = gradOutput:cdiv(self.output):div(2)
+   self.gradInput = gradOutput:cdiv(self.output_clone):div(2)
    return self.gradInput
 end
