0a069790fa2bdf4b35fc0de8664c6a7794776ccf
Jure Zbontar <jure.zbontar@gmail.com>
2014-07-09 21:26:54 +0200

    Dropout split tr and te phase

--- Dropout.lua
+++ Dropout.lua
@@ -2,17 +2,23 @@ local Dropout, Parent = torch.class('jzt.Dropout', 'nn.Module')
  
 function Dropout:__init(percentage)
    Parent.__init(self)
-   self.p = percentage or 0.5
-   if self.p > 1 or self.p < 0 then
-      error('<Dropout> illegal percentage, must be 0 <= p <= 1')
-   end
+   self.p = percentage
+   self.noise = torch.CudaTensor()
 end
  
 function Dropout:updateOutput(input)
-   self.noise = torch.rand(input:size()) -- uniform noise between 0 and 1
-   self.noise:add(1 - self.p):floor()  -- a percentage of noise
    self.output:resizeAs(input):copy(input)
-   self.output:cmul(self.noise)
+
+   -- stage is a global variable
+   if stage == 'train' then
+      self.noise:resizeAs(input)
+      self.noise:bernoulli(self.p)
+      self.output:cmul(self.noise)
+   elseif stage == 'test' then
+      self.output:mul(self.p)
+   else
+      assert(false)
+   end
    return self.output
 end
  
