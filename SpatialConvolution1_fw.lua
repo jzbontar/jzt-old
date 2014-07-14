a127cc49a3ac903b40534d9d9224b879e1971dfe
Jure Zbontar <jure.zbontar@gmail.com>
2014-07-14 22:45:39 +0200

    SpatialConvolution1_fw when batch size is larger than 1

--- SpatialConvolution1_fw.lua
+++ SpatialConvolution1_fw.lua
@@ -6,26 +6,28 @@ function SpatialConvolution1_fw:__init(inputSize, outputSize)
    parent.__init(self)
    self:cuda()
 
+   self.tmp_in = torch.CudaTensor()
+   self.tmp_out = torch.CudaTensor()
    self.weight = torch.CudaTensor(outputSize, inputSize)
    self.bias = torch.CudaTensor(1, outputSize, 1, 1)
 end
 
 function SpatialConvolution1_fw:updateOutput(input)
-   assert(input:size(1) == 1)
+   local num_ex = input:size(1)
    local fm_in = input:size(2)
    local h = input:size(3)
    local w = input:size(4)
    local fm_out = self.weight:size(1)
 
-   self.output:resize(fm_out, h * w)
-   input:resize(fm_in, h * w)
-   self.output:addmm(0, 1, self.weight, input)
-   input:resize(1, fm_in, h, w)
-   self.output:resize(1, fm_out, h, w)
+   self.tmp_in:resize(fm_in, h * w)
+   self.tmp_out:resize(fm_out, h * w)
+   self.output:resize(num_ex, fm_out, h, w)
+   for i = 1,num_ex do
+      self.tmp_in:copy(input[i])
+      self.tmp_out:addmm(0, 1, self.weight, self.tmp_in)
+      self.output[i]:copy(self.tmp_out)
+   end
 
-   self.bias:resize(1, fm_out, 1, 1)
    self.output:add(self.bias:expandAs(self.output))
-   self.bias:resize(fm_out)
-
    return self.output
 end
